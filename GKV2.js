61790809fa4636b5713d891642939955d898e352
Pjotterr <164270673+Pjotterr@users.noreply.github.com>
2024-09-21 00:06:15 +0200

    Add files via upload

---
+++ GKV2.js
@@ -0,0 +1,73 @@
+PanoramaStudioViewerParams = {
+"panoramaStudioViewer": {
+"key": "",
+"control": {
+        "mousemode": "drag",
+        "touchmode": "drag"
+    },
+"data": {
+        "id": "globalData",
+        "messageBoxStyle": "min-width: 25%; text-shadow: none; line-height: 100%; color:#ffffff; font-family: Arial,Helvetica,Sans-Serif; font-size: 15px; font-weight: 100; text-align:left; box-shadow: 4px 4px 1px 0px rgba(0,0,0,0.33);background: rgba(0,0,0,0.66);"
+    },
+"include": [
+        {
+            "src": "GKV2_controls.js"
+        }
+    ],
+"node": {
+	"autoplay": {
+	        "pan": 0.08,
+	        "reference": "screen",
+	        "restart": 5,
+	        "returnToLevel": true,
+	        "status": "off",
+	        "tilt": 0,
+	        "zoom": 1
+	    },
+	"camera": {
+	        "maxpan": 359.99999999999994,
+	        "maxtilt": 89.99999999999999,
+	        "minpan": 0,
+	        "mintilt": -89.99999999999999
+	    },
+	"hotspot": [
+	        {
+	            "anchor": "20,57",
+	            "bitmap": "pap_marker_red.png",
+	            "bitmapscale": "1.0;1.1",
+	            "bitmaptype": "floating",
+	            "href": "https://1drv.ms/f/s!ArfwTj_F2EwOgtxIg5Dw0FoX79gjFQ?e=0rZ1FO",
+	            "opacity": "1.00;1.00",
+	            "position": "180.1800,11.7900",
+	            "target": "_self",
+	            "textbox": {
+	                "align": "top",
+	                "style": "color:#ffffff; font-family: Arial, Helvetica, sans-serif; font-size: 12px; font-weight: normal; text-align:center; display: inline;",
+	                "text": "Gudrun Kolderups Veg 2"
+	            },
+	            "transition": "zoomin,2;blend,2",
+	            "unit": "deg"
+	        }
+	    ],
+	"hotspots": {
+	        "visible": true
+	    },
+	"id": "GKV2",
+	"image":{"baseindex":1,"bitmap":[{"back":{"src":"GKV2_tiles/GKV2_t_1b_%y_%x.jpg"},"down":{"src":"GKV2_tiles/GKV2_t_1d_%y_%x.jpg"},"front":{"src":"GKV2_tiles/GKV2_t_1f_%y_%x.jpg"},"height":3819,"left":{"src":"GKV2_tiles/GKV2_t_1l_%y_%x.jpg"},"right":{"src":"GKV2_tiles/GKV2_t_1r_%y_%x.jpg"},"tilesize":480,"up":{"src":"GKV2_tiles/GKV2_t_1u_%y_%x.jpg"},"width":3819},{"back":{"src":"GKV2_tiles/GKV2_t_2b_%y_%x.jpg"},"down":{"src":"GKV2_tiles/GKV2_t_2d_%y_%x.jpg"},"front":{"src":"GKV2_tiles/GKV2_t_2f_%y_%x.jpg"},"height":1910,"left":{"src":"GKV2_tiles/GKV2_t_2l_%y_%x.jpg"},"right":{"src":"GKV2_tiles/GKV2_t_2r_%y_%x.jpg"},"tilesize":480,"up":{"src":"GKV2_tiles/GKV2_t_2u_%y_%x.jpg"},"width":1910},{"back":{"src":"GKV2_tiles/GKV2_t_3b_%y_%x.jpg"},"down":{"src":"GKV2_tiles/GKV2_t_3d_%y_%x.jpg"},"front":{"src":"GKV2_tiles/GKV2_t_3f_%y_%x.jpg"},"height":955,"left":{"src":"GKV2_tiles/GKV2_t_3l_%y_%x.jpg"},"right":{"src":"GKV2_tiles/GKV2_t_3r_%y_%x.jpg"},"tilesize":480,"up":{"src":"GKV2_tiles/GKV2_t_3u_%y_%x.jpg"},"width":955},{"back":{"src":"GKV2_tiles/GKV2_t_4b_%y_%x.jpg"},"down":{"src":"GKV2_tiles/GKV2_t_4d_%y_%x.jpg"},"front":{"src":"GKV2_tiles/GKV2_t_4f_%y_%x.jpg"},"height":478,"left":{"src":"GKV2_tiles/GKV2_t_4l_%y_%x.jpg"},"right":{"src":"GKV2_tiles/GKV2_t_4r_%y_%x.jpg"},"tilesize":480,"up":{"src":"GKV2_tiles/GKV2_t_4u_%y_%x.jpg"},"width":478}],"multilevel":true,"preview":{"src":"GKV2_tiles/GKV2_preview.jpg"},"projection":"cubic"},
+	"textbox": [
+	        {
+	            "align": "top",
+	            "style": " color:#ffffff; font-family: Arial,Helvetica,sans-serif; text-shadow: 0px 0px 3.8px #000000, 0px 0px 2.5px #000000, 0px 0px 1.7px #000000; font-size: 12px; font-weight: bold; text-align:center;",
+	            "text": "Gudrun Kolderups Veg 2",
+	            "yoff": "4px"
+	        }
+	    ],
+	"view": {
+	        "hfov": 90,
+	        "pan": 180,
+	        "tilt": 0
+	    }
+},
+"version": "4.2"
+}
+}
